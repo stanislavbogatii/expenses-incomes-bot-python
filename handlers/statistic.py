b9830b342a37e2e3b68cec195d1d4ca45ef5014d
stanislavbogatii <stas.bogattiy@gmail.com>
2025-05-23 16:25:53 +0300

    stats periods

--- handlers/statistic.py
+++ handlers/statistic.py
@@ -1,16 +1,29 @@
 from repositories import TransactionRepository
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters.command import Command, CommandObject
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from utils import get_or_create_user
 from datetime import datetime, timedelta
 from keyboards import main_menu
 from models import UserModel
 from enums import TransactionType
+from keyboards import statistic_options_inline, back_to_stats_inline
+from dateutil.relativedelta import relativedelta
+
 
 transaction_repository = TransactionRepository()
 router = Router()
+
+stats_period_days = {
+    "day": relativedelta(days=1),
+    "week": relativedelta(weeks=1),
+    "month": relativedelta(months=1),
+    "three_months": relativedelta(months=3),
+    "six_months": relativedelta(months=6),
+    "year": relativedelta(years=1),
+    "all": None,
+}
 
 @router.message(Command(commands=['statistic']))
 async def cmd_get_statistic(message: Message, command: CommandObject, state: FSMContext):
@@ -40,5 +53,52 @@ async def cmd_get_statistic(message: Message, command: CommandObject, state: FSM
         f"Profit: {profit} mdl {'😔' if profit < 0 else '👍'}",
         reply_markup=main_menu,
     )
-    await state.clear()
+
+@router.callback_query(F.data == 'show_stats')
+async def cmd_add_expense(callback: CallbackQuery, state: FSMContext):
+    message = callback.message
+    id = message.from_user.id
+    username = message.from_user.username
+    await get_or_create_user(username=username, user_id=id)
+        
+    await callback.message.edit_text(
+        "Select interval for stats:",
+        reply_markup=statistic_options_inline
+    )
+    await callback.answer()
+
+@router.callback_query(F.data.startswith('stats_period_'))
+async def process_period(callback: CallbackQuery):
+    message = callback.message
+    period = callback.data.replace('stats_period_', '')
+    stats_period = stats_period_days[period]
+
+    id = message.from_user.id
+    username = message.from_user.username
+    user: UserModel = await get_or_create_user(username=username, user_id=id)
+
+    now = datetime.now()
+
+    if stats_period is None:
+        start_date = datetime.min 
+    else:
+        start_date = now - stats_period
+    transactions = await transaction_repository.find_all_by_interval(user_id=user.id, start_date=start_date, end_date=now)
+
+    income_sum = sum(t.amount for t in transactions if t.type == TransactionType.INCOME)
+    expense_sum = sum(t.amount for t in transactions if t.type == TransactionType.EXPENSE)
+    profit = income_sum - expense_sum
+
+    print("Start date:", start_date)
+    print("End date:", now)
+
+    await callback.message.edit_text(
+        f"{period} statistic: \n\n" \
+        f"Expense: {expense_sum} mdl\n" \
+        f"Income: {income_sum} mdl\n\n" \
+        f"Profit: {profit} mdl {'😔' if profit < 0 else '👍'}",
+        reply_markup=back_to_stats_inline
+    )
+    await callback.answer()
+
 
--- keyboards.py
+++ keyboards.py
@@ -9,6 +9,28 @@ main_menu_inline = InlineKeyboardMarkup(
     ]
 )
 
+back_to_stats_inline = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='<< BACK', callback_data='show_stats')],
+    ]
+)
+
+statistic_options_inline = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Day', callback_data='stats_period_day'),
+            InlineKeyboardButton(text='Week', callback_data='stats_period_week'),
+            InlineKeyboardButton(text='Month', callback_data='stats_period_month')
+        ],
+        [
+            InlineKeyboardButton(text='3 Month', callback_data='stats_period_three_months'),
+            InlineKeyboardButton(text='6 Month', callback_data='stats_period_six_months'),
+            InlineKeyboardButton(text='Year', callback_data='stats_period_year')
+        ],
+        [InlineKeyboardButton(text='All time', callback_data='stats_period_all')],
+    ]
+)
+
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Add income'), KeyboardButton(text='Add expense')],
